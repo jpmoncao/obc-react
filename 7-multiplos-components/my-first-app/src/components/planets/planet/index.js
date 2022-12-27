1ccc6e073f360f4ca9f619f9f1168067ec988dba
João Pedro Monção <pedrohjoao44@gmail.com>
2022-12-27 17:52:46 -0300

    📒 Aula sobre renderização de múltiplos components

--- 7-multiplos-components/my-first-app/src/components/planets/planet/index.js
+++ 7-multiplos-components/my-first-app/src/components/planets/planet/index.js
@@ -5,6 +5,8 @@ import { GrayImg } from '../../shared/GrayImg'
 
 
 export const Planet = props => {
+    const names = ['a', 'b', 'c', 'd', 'e', 'f']
+
     let title
 
     if (props.titleWithUnderline)
@@ -18,6 +20,15 @@ export const Planet = props => {
             {title}
             <DescriptionWithLink text={props.desc} descUrl={props.descUrl} /> <br />
             <GrayImg imgUrl={props.imgUrl} gray={props.grayImg} />
+
+            <h4>Satélites</h4>
+            <ul>
+                {
+                    names.map(name =>
+                        <li>Satélite {name}</li>
+                    )
+                }
+            </ul>
         </div>
     )
 }
